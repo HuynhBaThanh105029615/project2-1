ccd28520345f6932ce9da2c2d566c6d2c26b2cc5
HuynhBaThanh105029615 <105029615@student.swin.edu.au>
2025-03-17 21:36:59 +0700

    my message

---
+++ about.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+    include 'header.inc';
+    include 'menu.inc';
+    include 'footer.inc';
+?>
+<body class="about-body">
+    <article>
+        <dl id="brief" class="about-us">
+            <dt id="term1" class="about-us">About us</dt>
+            <dd id="introduction" class="about-us">We are <b>Team 4TL</b>, a group of four dedicated developers working together to build a fully functional website. Each of our four members takes responsibility for designing and coding a specific page, ensuring a well-structured and seamless user experience. Through teamwork, creativity, and technical skills, we aim to create a website that is both visually appealing and highly functional.</dd>
+            <dt id="term2" class="tutor">Tutor</dt>
+            <dd id="tutor">Tristian Trung Nguyen</dd>
+            <dt id="term3" class="slogan">Slogan</dt>
+            <dd id="slogan">4TL will never fail</dd>
+        </dl>
+        <div class="memberinfo">
+        <h2 id="ourmembers">Members</h2>
+        <img id="pic1" class="picture" src="images/z4038972986113_b36cfd70c0449b1e9b51fa1f94b67f90.jpg" 
+            alt="picture of Huynh Ba Thanh"
+            height="400"
+            width="260">
+        <img id="pic2" class="picture" src="images/6505939f7d98c3c69a89.jpg"
+            alt="picture of Ho Thanh Phong"
+            height="400"
+            width="260">
+        <img id="pic3" class="picture" src="images/cdbbc07ddc7f62213b6e.jpg"
+            alt="picture of Nguyen Luong Thien"
+            height="400"
+            width="260">
+        <img id="pic4" class="picture" src="images/z6332054821629_7799417a8afbeffffeebc8d0244758dd.jpg"
+            alt="picture of Lai Vu Chi An"
+            height="400"
+            width="260">
+        <dl id="member1">
+            <dt>Name:</dt>
+            <dd>Huynh Ba Thanh</dd>
+            <dt>Date of Birth:</dt>
+            <dd>14/08/2005</dd>
+            <dt>Hometown:</dt>
+            <dd>Ho Chi Minh</dd>
+            <dt>Task:</dt>
+            <dd>about.html</dd>
+            <dt>Skills:</dt>
+            <dd>
+                <div class="progress-container">
+                    <div class="progress-bar-8" style="width: 80%;">8</div>
+                </div>
+            </dd>
+            <dt>Teamwork:</dt>
+            <dd>
+                <div class="progress-container">
+                    <div class="progress-bar-10" style="width: 100%;">10</div>
+                </div>
+            </dd>
+        </dl>
+        <dl id="member2">
+            <dt>Name:</dt>
+            <dd>Ho Thanh Phong</dd>
+            <dt>Date of Birth:</dt>
+            <dd>23/08/2005</dd>
+            <dt>Hometown:</dt>
+            <dd>Thua Thien Hue</dd>
+            <dt>Task:</dt>
+            <dd>jobs.html</dd>
+            <dt>Skills:</dt>
+            <dd>
+                <div class="progress-container">
+                    <div class="progress-bar-9" style="width: 90%;">9</div>
+                </div>
+            </dd>
+            <dt>Teamwork:</dt>
+            <dd>
+                <div class="progress-container">
+                    <div class="progress-bar-10" style="width: 100%;">10</div>
+                </div>
+            </dd>
+        </dl>
+        <dl id="member3">
+            <dt>Name:</dt>
+            <dd>Nguyen Luong Thien</dd>
+            <dt>Date of Birth:</dt>
+            <dd>07/12/2005</dd>
+            <dt>Hometown:</dt>
+            <dd>Ho Chi Minh</dd>
+            <dt>Task:</dt>
+            <dd>apply.html</dd>
+            <dt>Skills:</dt>
+            <dd>
+                <div class="progress-container">
+                    <div class="progress-bar-9" style="width: 90%;">9</div>
+                </div>
+            </dd>
+            <dt>Teamwork:</dt>
+            <dd>
+                <div class="progress-container">
+                    <div class="progress-bar-10" style="width: 100%;">10</div>
+                </div>
+            </dd>
+        </dl>
+        <dl id="member4">
+            <dt>Name:</dt>
+            <dd>Lai Vu Chi An</dd>
+            <dt>Date of Birth:</dt>
+            <dd>25/07/2006</dd>
+            <dt>Hometown:</dt>
+            <dd>Ho Chi Minh</dd>
+            <dt>Task:</dt>
+            <dd>index.html</dd>
+            <dt>Skills:</dt>
+            <dd>
+                <div class="progress-container">
+                    <div class="progress-bar-9" style="width: 90%;">9</div>
+                </div>
+            </dd>
+            <dt>Teamwork:</dt>
+            <dd>
+                <div class="progress-container">
+                    <div class="progress-bar-10" style="width: 100%;">10</div>
+                </div>
+            </dd>
+        </dl>
+        </div>
+        <div class="timetable">
+        <h2>Our timetable</h2>
+        <table>
+            <tr>
+                <th>Time</th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+                <th>Sunday</th>
+            </tr>
+            <tr>
+                <th>7am</th>
+                <td rowspan="5">TNE10006<br>7am-11am</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>8am</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>9am</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>10am</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td rowspan="4">STA10003<br>10am-1pm</td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>11am</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>12pm</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>1pm</th>
+                <td></td>
+                <td></td>
+                <td rowspan="5">COS10026<br>1pm-5pm</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>2pm</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>3pm</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>4pm</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th>5pm</th>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+        </div>
+        <div class="contact-us">
+        <h2>Contact us</h2>
+        <p id="phrasecontact">If you have any question, don't hesitate to contact us via this <b><i><a href="mailto:thanh140820522@gmail.com">email</a></i></b></p>
+        </div>
+    </article>
+</body>
+    
