e4f7621702bbfd27b259bd4680497e49f6f9d451
HuynhBaThanh105029615 <105029615@student.swin.edu.au>
2025-03-19 16:59:30 +0700

    process

--- apply.php
+++ apply.php
@@ -12,53 +12,53 @@
     <p id="p1">Please Fill out the form Bellow to submit Your Job Application!</p>
     <br>
     <br>
-<form method="post" action="process_eoi.php">
+<form method="POST" action="process_eoi.php">
     <fieldset>
         <legend>Job reference number</legend>
     <br>
-    <input type="text" placeholder="J97" maxlength="5" pattern="[a-zA-Z0-9]+" required="required" name="Job reference number" id="job_ref" >
+    <input type="text" placeholder="J97" maxlength="5" pattern="[a-zA-Z0-9]+" required="required" name="job_ref" id="job_ref" >
 </fieldset><br>
     <br>
     <fieldset>
         <legend>Name:</legend>
 <br>
-    <input type="text" placeholder=" First name" size="50" width="50"  required="required" maxlength="20" name="First Name" id="first_name" />   
+    <input type="text" placeholder=" First name" size="50" width="50"  required="required" maxlength="20" name="first_name" id="first_name" />   
     <br>
     <br>
     <br>
-    <input type="text" placeholder=" Last name" size="50" width="50"  required="required"  maxlength="20" name="Last name" id="last_name"/> 
+    <input type="text" placeholder=" Last name" size="50" width="50"  required="required"  maxlength="20" name="last_name" id="last_name"/> 
 </fieldset>
     <br>
     <br>
     <fieldset>
         <legend>Date of birth</legend>
     <br>
-    <input type="date" placeholder=" dd/mm/yyyy"  required="required" size="50" width="50" name="date" id="dob"/>  
+    <input type="date" placeholder=" dd/mm/yyyy"  required="required" size="50" width="50" name="dob" id="dob"/>  
 </fieldset>
     <br>
     <fieldset>
         <legend>Gender:</legend>
         
-    <input type="radio"  required="required" name="Gender"><i id="gioitinh"> Male </i> 
-    <input type="radio"  name="Gender"> <i id="gioitinh"> Female </i>
-     <input type="radio" name="Gender"><i id="gioitinh"> Others </i> <br>
+    <input type="radio"  required="required" name="gender"><i id="gioitinh"> Male </i> 
+    <input type="radio"  name="gender"> <i id="gioitinh"> Female </i>
+     <input type="radio" name="gender"><i id="gioitinh"> Others </i> <br>
 </fieldset>
 <br>
 <fieldset>
     <legend>Street Address</legend>
 <br>
-<input type="text" size="50" width="50"  maxlength="40" name="Street Address"  required="required" id="street"/>
+<input type="text" size="50" width="50"  maxlength="40" name="street"  required="required" id="street"/>
 </fieldset>
 <br>
 <fieldset>
 <legend>Suburb/town</legend>
 <br>
-<input type="text" size="50" width="50"  maxlength="40" name="Suburb/town" required="required" id="suburb"/>
+<input type="text" size="50" width="50"  maxlength="40" name="suburb" required="required" id="suburb"/>
 </fieldset>
 <br>
 <br>
-<label>State: </label>
-<select name="State" required="required" id="state" >
+<label for="state">State: </label>
+<select name="state" required="required" id="state" >
     <option value="">Please select your State</option>
     <option value="VIC">VIC </option>
     <option value="NSW">NSW</option>
@@ -76,35 +76,35 @@
     <br>
     <br>
     <br>
-    <input type="text" placeholder=" 2wn9hxh1" size="4" width="50" required="required" pattern="[0-9]+" name="PostCode"  required="required" id="postcode"/>   <br>
+    <input type="text" placeholder=" 2wn9hxh1" size="4" width="50" required="required" pattern="[0-9]+" name="postcode"  required="required" id="postcode"/>   <br>
     <br>
     <label>E-mail: </label>
     <br><br><br>
-    <input type="email" placeholder=" nguyenluongthien@gmail.com" name="E-mail"  required="required" id="email"/>
+    <input type="email" placeholder=" nguyenluongthien@gmail.com" name="email"  required="required" id="email"/>
     <br>
     <br>
     <div>  
     <label>Phone number</label>
     <br><br><br>
-    <input type="text" placeholder=" 0464398624t4" maxlength="12" pattern="[0-9 ]{8,12}" name="Phone number"  required="required" id="phone"/>
+    <input type="text" placeholder=" 0464398624t4" maxlength="12" pattern="[0-9 ]{8,12}" name="phone"  required="required" id="phone"/>
     <br>
     <br>
     <fieldset>
         <legend>Skill list</legend>
-        <input type="checkbox" name="Skills[]"><i id="kynang"> Team Work </i>
+        <input type="checkbox" name="skills[]"><i id="kynang"> Team Work </i>
         <br>
-        <input type="checkbox" name="Skills[]"><i id="kynang"> Marketing </i>
+        <input type="checkbox" name="skills[]"><i id="kynang"> Marketing </i>
         <br>
-         <input type="checkbox" name="Skills[]"><i id="kynang"> Microsoft </i><br>
+         <input type="checkbox" name="skills[]"><i id="kynang"> Microsoft </i><br>
          <br>
          <fieldset>
             <legend>Other skills: </legend>
          <br>
          <textarea 
-         name="Skills[]"
+         name="other_skills[]"
          rows="10"
          cols="30S"
-         id="other_skill"
+         id="other_skills"
          >
           
          </textarea>
--- process_eoi.php
+++ process_eoi.php
@@ -10,6 +10,7 @@
 </head>
 <body>
 <?php
+    
     require_once "settings.php";
     $conn = @mysqli_connect($host, $user, $pwd, $sql_db); 
 
@@ -19,20 +20,21 @@
         exit();
     }
     
+
     //Sanitize and validate input
-    $job_ref = trim($_POST['job_ref']);
-    $first_name = trim($_POST['first_name']);
-    $last_name = trim($_POST['last_name']);
-    $dob = trim($_POST['dob']);
-    $gender = trim($_POST['gioitinh']);
-    $street = trim($_POST['street']);
-    $suburb = trim($_POST['suburb']);
-    $state = trim($_POST['state']);
-    $postcode = trim($_POST['postcode']);
-    $email = trim($_POST['email']);
-    $phone = trim($_POST['phone']);
-    $skill = isset($_POST['kynang']) ? $_POST['kynang'] : [];
-    $other_skill = trim($_POST['other_skill']);
+    $job_ref = htmlspecialchars($_POST['job_ref']);
+    $first_name = htmlspecialchars($_POST['first_name']);
+    $last_name = htmlspecialchars($_POST['last_name']);
+    $dob = htmlspecialchars($_POST['dob']);
+    $gender = htmlspecialchars($_POST['gender']);
+    $street = htmlspecialchars($_POST['street']);
+    $suburb = htmlspecialchars($_POST['suburb']);
+    $urstate = htmlspecialchars($_POST['state']);
+    $postcode = htmlspecialchars($_POST['postcode']);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    $skill = isset($_POST['skills']) ? implode(", ", $_POST['skills']) : ""; // Convert array to string
+    $other_skills = htmlspecialchars($_POST['other_skills']);
     $status = "New";
 
     // Server-side validation
@@ -40,12 +42,11 @@
     if (!preg_match("/^[a-zA-Z0-9]{5}$/", $job_ref)) $errors[] = "Invalid Job Reference Number.";
     if (!preg_match("/^[a-zA-Z]{1,20}$/", $first_name)) $errors[] = "Invalid First Name.";
     if (!preg_match("/^[a-zA-Z]{1,20}$/", $last_name)) $errors[] = "Invalid Last Name.";
-    if (!preg_match("/^\d{2}\/\d{2}\/\d{4}$/", $dob)) $errors[] = "Invalid Date of Birth.";
-    if (!in_array($state, ["VIC", "NSW", "QLD", "NT", "WA", "SA", "TAS", "ACT"])) $errors[] = "Invalid State.";
+    if (!in_array($urstate, ["VIC", "NSW", "QLD", "NT", "WA", "SA", "TAS", "ACT"])) $errors[] = "Invalid State.";
     if (!preg_match("/^\d{4}$/", $postcode)) $errors[] = "Invalid Postcode.";
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) $errors[] = "Invalid Email.";
     if (!preg_match("/^[0-9 ]{8,12}$/", $phone)) $errors[] = "Invalid Phone Number.";
-    if (empty($skills) && empty($other_skills)) $errors[] = "You must enter some skills.";
+    if (empty($skills)) $errors[] = "You must enter some skills.";
 
     if (!empty($errors)) {
         foreach ($errors as $error) {
@@ -57,8 +58,8 @@
     //adding process
 
     $sql_table = "eoi";
-    $query = "INSERT INTO $sql_table (job_ref_num, first_name, last_name, street, suburb, urstate, postcode, email, phone, skill, other_skill, status) VALUES ('$job_ref', '$first_name', '$last_name', '$street',
-    '$suburb', '$urstate', '$postcode', '$email', '$phone', '$skill', '$other_skill', '$street', '$status')";
+    $query = "INSERT INTO $sql_table (job_ref_num, first_name, last_name, street, suburb, urstate, postcode, email, phone, skills, other_skills, status) VALUES ('$job_ref', '$first_name', '$last_name', '$street',
+    '$suburb', '$urstate', '$postcode', '$email', '$phone', '$skills', '$other_skills', '$street', '$status')";
     $result = mysqli_query($conn, $query);
     if (!$result) {
         echo "<p class=\"wrong\">Something in wrong with ", $query, "</p>";
