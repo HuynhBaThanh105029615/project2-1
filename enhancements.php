99cba5db9d8f4c6d4c235bbf771ccc78179acff5
HuynhBaThanh105029615 <105029615@student.swin.edu.au>
2025-03-19 13:29:37 +0700

    enhancephp

---
+++ enhancements.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    include 'header.inc'
+?>
+    <!-- Header -->
+    <?php
+        include 'menu.inc'
+    ?>
+    <article>
+        <h1>Enhancement:</h1>
+        <p class="text1">Our website features a sleek and fully responsive navigation bar designed to enhance user experience. Whether viewed on a desktop or resized browser window, the navigation bar adapts seamlessly, ensuring easy access to key pages. With a clean design, intuitive menu structure, and smooth transitions, users can navigate effortlessly. This responsive approach ensures that visitors always enjoy a smooth and engaging browsing experience</p>
+    </article>
+    <img src="images/pic1.png" alt="picture of the responsive nav bar" class="imgen">
+    <img src="images/pic2.png" alt="picture of the responsive nav bar 2" class="imgen">
+   <?php
+        include 'footer.inc'
+    ?>
+
+</body>
