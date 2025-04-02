b616e4c378a1d612ad607c91b54419980fabdca2
HuynhBaThanh105029615 <105029615@student.swin.edu.au>
2025-04-02 11:22:13 +0700

    registerprocess

--- register.php
+++ register.php
@@ -7,7 +7,17 @@
     <?php
         include 'menu.inc';
     ?>
-
+    <div class="registerform">
+        <form method="POST" action="register_process.php">
+            <Label for="fullname">Please enter your full name:</Label>
+            <input type="text" name="fullname" required>
+            <label for="username">Username:</label>
+            <input type="text" name="username" required>
+            <label for="password">Password:</label>
+            <input type="text" name="password" required>
+            <input type="submit" value="Register" name="createaccount" id="createaccountbutton">
+        </form>
+    </div>
     <?php
         include 'footer.inc';
     ?>
---
+++ register_process.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Register</title>
+
+<meta charset="utf-8" />
+<meta name="description" content="project2"  />
+<meta name="keywords" content="register" />
+
+</head>
+<body>
+<?php
+    
+    require_once "settings.php";
+    $conn = @mysqli_connect($host, $user, $pwd, $sql_db); 
+    
+    //prevent direct access by url
+    if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+        header("Location: apply.php");
+        exit();
+    }
+    
+
+    //Sanitize and validate input
+    $fullname = htmlspecialchars(trim($_POST['fullname']));
+    $username = htmlspecialchars(trim($_POST['username']));
+    $password = htmlspecialchars(trim($_POST['password']));
+
+    // Server-side validation
+    $errors = [];
+    if (!preg_match("/^[a-zA-Z0-9 ]{1,50}$/", $fullname)) $errors[] = "Invalid Full Name.";
+    if (!preg_match("/^[a-zA-Z0-9 ]{1,50}$/", $username)) $errors[] = "Invalid Username.";
+    if (!preg_match("/^[a-zA-Z0-9 ]{1,50}$/", $password)) $errors[] = "Invalid Password.";
+
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo "<p>$error</p>";
+        }
+        exit();
+    }
+
+    //adding process
+
+    $sql_table = "manage";
+    $query = "INSERT INTO $sql_table (fullname, username, password) VALUES ('$fullname','$username','$password')";
+    $result = mysqli_query($conn, $query);
+    if (!$result) {
+        echo "<p class=\"wrong\">Something in wrong with ", $query, "</p>";
+    }   else {
+        echo "<p class=\"ok\">Successfully added new account</p>";
+        echo "<p>You can back to the login <a href='manage_login.php'>here</a></p>";
+    }
+    mysqli_close($conn);
+
+?>
+</body>
